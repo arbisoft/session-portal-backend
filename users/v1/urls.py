f6b7dfda87d3a96dc8f7666cbb4afaf9464a78b6
Muhammad Naveed Khan <muhammad.naveed@arbisoft.com>
2024-11-21 19:59:47 +0500

    added register and login functionality.

--- users/v1/urls.py
+++ users/v1/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from users.v1.views import LoginUserView, HelloWorldView
+from users.v1.views import LoginUserView, HelloWorldView, RegisterUserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('login', LoginUserView.as_view(), name='login_user'),
-    path('hello', HelloWorldView.as_view(), name='hello_world'),
+    path('register/', RegisterUserView.as_view(), name='register_user'),
+    path('login/', LoginUserView.as_view(), name='login_user'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('hello/', HelloWorldView.as_view(), name='hello_world'),
 ]
--- users/v1/views.py
+++ users/v1/views.py
@@ -1,13 +1,46 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.contrib.auth import authenticate
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.exceptions import ValidationError
 from rest_framework_simplejwt.tokens import RefreshToken
+from users.serializers import  UserSerializer, RegisterUserSerializer, LoginUserSerializer
 
-from users.v1.utils import get_google_user_info
+# from users.v1.utils import get_google_user_info
+# user_model = get_user_model()
 
-user_model = get_user_model()
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterUserSerializer
 
+
+@method_decorator(csrf_exempt, name='dispatch')
+class LoginUserView(APIView):
+    serializer_class = LoginUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(username=email, password=password)  # Use `username` for Django's default User model
+        if not user:
+            return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        refresh = RefreshToken.for_user(user)
+        user_serializer = UserSerializer(user)
+
+        return Response({
+            'token': str(refresh.access_token),
+            'user': user_serializer.data
+        }, status=status.HTTP_200_OK)
+
+
+
+"""  
 class LoginUserView(APIView):
 
     permission_classes = []
@@ -24,9 +57,13 @@ class LoginUserView(APIView):
             'refresh': str(refresh),
             'access': str(refresh.access_token),
         })
+"""
 
 
 class HelloWorldView(APIView):
 
     def get(self, request):
         return Response("Hello World")
+
+
+
