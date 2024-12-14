a915e44938b82c69094571b389600964631f6d80
Muhammad Aleem Nadeem <muhammad.aleem@A006-01546.local>
2024-12-14 18:38:23 +0500

    added events type view

--- events/v1/views.py
+++ events/v1/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from events.models import Event
+from .serializers import EventTypeSerializer
+
+class EventTypeListView(APIView):
+    def get(self, request, *args, **kwargs):
+        event_type = Event.EventType.choices
+        serializer = EventTypeSerializer(event_type, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
