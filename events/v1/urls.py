929ea5de3ff8e931c6d4ec3ea7a28356ce0843a4
Muhammad Aleem Nadeem <muhammad.aleem@A006-01546.local>
2024-12-14 18:37:49 +0500

    added event type api url

--- events/v1/urls.py
+++ events/v1/urls.py
@@ -1,1 +1,6 @@
-# Create you urls here.
+from django.urls import path
+from .views import EventTypeListView
+
+urlpatterns = [
+    path('event_types/', EventTypeListView.as_view(), name='event-type-list'),
+]
