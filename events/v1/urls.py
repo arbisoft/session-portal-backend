77273435d1ea400efacb4d92f1ee3944d762ec36
Muhammad Aleem Nadeem <muhammad.aleem@A006-01546.local>
2025-01-14 22:47:47 +0500

    added video-asset

--- events/v1/urls.py
+++ events/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import EventTypeListView, EventsListView
+from .views import EventTypeListView, EventsListView, VideoAssetListView
 
 urlpatterns = [
     path('event_types/', EventTypeListView.as_view(), name='event-type-list'),
-    path('all/', EventsListView.as_view(), name='events-list')
+    path('all/', EventsListView.as_view(), name='events-list'),
+    path('videoasset/<int:event_id>/', VideoAssetListView.as_view(), name='video-asset')
 ]
