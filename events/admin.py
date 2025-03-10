20e232340c50a646df100b6b19a99686a6725c2a
Arslan Ather <arslan.ather@arbisoft.com>
2025-03-11 02:26:08 +0500

    feat: make several improvements to admin

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import TextInput, Textarea
 
 from events.forms import VideoAssetForm
 from events.models import Event, Tag, VideoAsset
@@ -12,6 +14,9 @@ class VideoAssetInline(admin.StackedInline):
     max_num = 1
     min_num = 1
     can_delete = False
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'style': 'width: 100%'})},
+    }
     # WIP: Automatically detect the file_size and video duration.
     # readonly_fields = ('file_size', 'duration')
 
@@ -22,7 +27,15 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'event_type', 'status', 'event_time', 'creator')
     list_filter = ('event_type', 'status', 'event_time')
     search_fields = ('title', 'description')
+    filter_horizontal = ('tags', )
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'style': 'width: 100%'})},
+        models.TextField: {'widget': Textarea(attrs={'style': 'width: 100%'})},
+    }
     inlines = [VideoAssetInline]
+    autocomplete_fields = ('creator', )
+    ordering = ('event_time', 'title', 'status')
+    list_editable = ('status', )
 
     fieldsets = (
         (None, {
